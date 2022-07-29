8d0a9660ccb643a6632b49740e5cc3cfc124fb5c
Tino <miltonjacomini@gmail.com>
2022-07-29 11:50:30 -0300

    Commit Day 4 do desafio 7 Days of Code

--- src/main/java/br/com/alura/dayscode/FilmeController.java
+++ src/main/java/br/com/alura/dayscode/FilmeController.java
@@ -1,5 +1,7 @@
 package br.com.alura.dayscode;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.ContextLoader;
 
 @RestController
 public class FilmeController {
@@ -17,10 +20,14 @@ public class FilmeController {
     private RestTemplate restTemplate;
 
     @GetMapping("/top250")
-    public ListOfMovies getTop250Filmes() {
+    public ListOfMovies getTop250Filmes() throws FileNotFoundException {
 
         ResponseEntity<ListOfMovies> response =
                 this.restTemplate.getForEntity("https://imdb-api.com/en/API/Top250Movies/k_puevtnbr", ListOfMovies.class);
+
+        PrintWriter writer = new PrintWriter("src/main/resources/content.html");
+        new HTMLGenerator(writer).generate(response.getBody());
+        writer.close();
 
         return response.getBody();
     }
